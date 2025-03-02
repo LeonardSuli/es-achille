20933364778261b203ab0577e92876c218c71c8f
LeonardSuli <leonardsuli1991@gmail.com>
2025-03-02 21:45:32 +0100

    add event listener on button ferma

--- es_3/script.js
+++ es_3/script.js
@@ -1,5 +1,30 @@
 
-const button = document.querySelector('button');
 const div = document.querySelector('div');
 const input = document.querySelector('input');
+const scorri = document.querySelector('#scorri');
+const ferma = document.querySelector('#ferma');
 
+let timer;
+
+// function scorrimento() {
+scorri.addEventListener('click', () => {
+
+    div.innerText = input.value;
+
+    timer = setInterval(() => {
+
+        const letteraIniziale = div.innerText.slice(0, 1);
+        const restoParola = div.innerText.slice(1);
+        console.log(restoParola + letteraIniziale)
+
+        div.innerText = restoParola + letteraIniziale;
+
+    }, 500);
+
+})
+// }
+
+ferma.addEventListener('click', () => {
+
+    clearInterval(timer);
+});
