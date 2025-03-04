831bd5c57ce1eeec00668f8a919a2ad6a026309c
LeonardSuli <leonardsuli1991@gmail.com>
2025-03-04 18:14:05 +0100

    add container empty when i click button genera

--- es_4/script.js
+++ es_4/script.js
@@ -4,26 +4,22 @@ let output = document.querySelector('.output');
 
 container.style.display = 'none';
 
-button.addEventListener('click', () => {
 
+button.addEventListener('click', () => {
 
     // compare il contenitore
     container.style.display = 'flex';
 
+    // i box resettano ogni volta che fai click e non raddoppiano
+    container.innerHTML = '';
 
     for (let i = 1; i < 11; i++) {
-        console.log(i)
+        // console.log(i)
 
         let numRandom = Math.ceil(Math.random() * 10);
 
         container.innerHTML += `<div class="output">${numRandom}</div>`;
 
     }
-
-    // i box resettano ogni volta che fai click
-    // output.innerHTML = '';
-
-    // // aggiunge un numero random
-    // output.innerText += Math.ceil(Math.random() * 10);
 
 })
